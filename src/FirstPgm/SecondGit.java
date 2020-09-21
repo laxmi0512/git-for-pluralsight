3352b2f7c2e5d0cccdf38f788f66b31d4416a055
laxmi <lachusl1219@gmail.com>
2020-09-21 18:24:55 +0530

    added getter setter for age

--- src/FirstPgm/SecondGit.java
+++ src/FirstPgm/SecondGit.java
@@ -4,6 +4,12 @@ public class SecondGit {
 
 	private String name;
 	private int age;
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
 	public String getname() {
 		return name;
 	}
